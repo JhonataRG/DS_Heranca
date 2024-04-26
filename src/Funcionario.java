3c10096ca1710d62577e3db51f6830f5e48b7408
JhonataRG <163172590+JhonataRG@users.noreply.github.com>
2024-04-26 09:59:34 -0300

    Add files via upload

---
+++ src/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario extends PessoaFisica{
+    private String cartao;
+    public void setcartao(String cartao){
+        this.cartao = cartao;
+    }
+    public String getCartao(){
+    return (cartao);
+    }
+}
+
+
